1c19c11b16329dbe2d3fc3a1c2339df222d2ba1b
Honey Chauhan <hc924508@gmail.com>
2023-04-29 02:18:17 +0530

    3rd sub

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -2,6 +2,7 @@ package com.driver;
 
 import java.util.List;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
+@Log4j2
 public class OrderController {
 
     @Autowired
     OrderService orderService;
 
+    public OrderController(){
+        log.info("running controller");
+    }
+
     public OrderController(OrderService orderService){
+        log.info("running controller");
         this.orderService = orderService;
     }
 
